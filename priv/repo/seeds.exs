c684d9013dcdd6ed3bd9e5545374d641481f8f02
Dimitrios Stergiou <dimitrios.stergiou@trustly.com>
2019-12-09 12:23:50 +0100

    added seed file for L15

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ElixirTrustlyctf.Repo
+Repo.insert!(%ElixirTrustlyctf.Content{name: "Per", data: "This is a test"})
+Repo.insert!(%ElixirTrustlyctf.Content{name: "Dimitrios", data: "I made this"})
+Repo.insert!(%ElixirTrustlyctf.Content{name: "Nick", data: "The Greek"})
+Repo.insert!(%ElixirTrustlyctf.Content{name: "flag", data: "flag{lego}"})
