6e71821b6cd518d1350f9062da1e972b9ca297fc
Dimitrios Stergiou <dimitrios.stergiou@trustly.com>
2019-12-10 15:21:51 +0100

    L16

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -8,20 +8,27 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    //'./js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    'app': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js')),
+    'sixteen': ['./js/sixteen.js'].concat(glob.sync('./vendor/**/*.js')),
+
   },
   output: {
-    filename: 'app.js',
+    // filename: 'app.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../priv/static/js')
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -35,7 +42,12 @@ module.exports = (env, options) => ({
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new MiniCssExtractPlugin({
+      filename: '../css/app.css'
+    }),
+    new CopyWebpackPlugin([{
+      from: 'static/',
+      to: '../'
+    }])
   ]
 });
